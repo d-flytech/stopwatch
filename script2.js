d628422a5a35ff92c7d7ca6ddc8838e36de05a86
d-flytech <desirevanbrummen@hotmail.com>
2024-09-27 21:39:11 +0200

    seperate functions deleted

--- script2.js
+++ script2.js
@@ -1,29 +1,14 @@
 import Stopwatch from './script.js';
 
-const stopwatch = new Stopwatch();
-
-
-// Setting up event listeners
 window.addEventListener("DOMContentLoaded", () => {
-    initializeStopwatch();
-    document.getElementById('start').addEventListener('click', startStopwatch);
-    document.getElementById('stop').addEventListener('click', stopStopwatch);
-    document.getElementById('reset').addEventListener('click', resetStopwatch);
+    const stopwatchInstance = new Stopwatch();
+    stopwatchInstance.initializeStopwatch();
+   
+    stopwatchInstance.start_btn.addEventListener('click', () => stopwatchInstance.start());
+    stopwatchInstance.stop_btn.addEventListener('click', () => stopwatchInstance.stop());
+    stopwatchInstance.reset_btn.addEventListener('click', () => stopwatchInstance.reset());
+    stopwatchInstance.lap_btn.addEventListener('click', () => stopwatchInstance.lap());
+
 });
 
-// Function definitions
-function startStopwatch() {
-    stopwatch.start();
-}
 
-function stopStopwatch() {
-    stopwatch.stop();
-}
-
-function resetStopwatch() {
-    stopwatch.reset();
-}
-
-function initializeStopwatch() {
-    stopwatch.initializeLaps();
-}
